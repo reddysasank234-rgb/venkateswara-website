b95d79e850fda29cd694bfa890b6aa3ae6b29224
reddysasank234-rgb <reddysasank234@gmail.com>
2026-03-11 23:59:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function toggleAudio(){
 
+let audio = document.getElementById("chant");
+
+if(audio.paused){
+audio.play();
+}else{
+audio.pause();
+}
+
+}
+
+function toggleMenu(){
+
+let menu = document.getElementById("menu");
+
+if(menu.style.display === "block"){
+menu.style.display = "none";
+}else{
+menu.style.display = "block";
+}
+
+}
+
+function setSpeed(speed){
+
+let audio = document.getElementById("chant");
+audio.playbackRate = speed;
+
+}
